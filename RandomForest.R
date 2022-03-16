327a4e796bb984214883c0bf79fce11bbe682b51
David <david.izrailov11@gmail.com>
2022-03-16 14:49:08 -0400

    added variables spread1,spread2 and updated graphs

--- RandomForest.R
+++ RandomForest.R
@@ -1,13 +1,14 @@
 #import libraries
 library(tidyverse)
 library(randomForest)
+library(caTools)
 
 #dataset from https://archive.ics.uci.edu/ml/datasets/parkinsons
 
 #import data
-set.seed(13)
+set.seed(639)
 data<-read.csv(file="/Users/david/Desktop/Code/R/Parkinsons Prediction/parkinsons.csv")%>%
-  select(-c(name,spread1,spread2))%>%
+  select(-c(name))%>%
   mutate(status=factor(status,
                        levels=c(0,1),
                        labels=c("negative","positive")))
@@ -27,3 +28,4 @@ varImpPlot(rf,main = "Random Forest Variable Importance")
 p<-predict(rf,test1)
 table(test1[,17],p)
 mean(p==test1[,17])
+
